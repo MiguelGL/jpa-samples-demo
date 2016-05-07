5547ca14a6f9eafa89f7926bd47b35a5aa2d2f8a
Miguel García Lpez <miguel.garcia.lopez@gmail.com>
2016-05-07 02:36:59 +0200

    Using validation annotations to generate schema

--- mapping-samples/src/main/java/com/mgl/jpa/mapping/samples/UserProfile.java
+++ mapping-samples/src/main/java/com/mgl/jpa/mapping/samples/UserProfile.java
@@ -1,9 +1,14 @@
 package com.mgl.jpa.mapping.samples;
 
+import java.util.Date;
+
 import javax.persistence.Basic;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import com.mgl.jpa.mapping.samples.contact.ContactInformation;
 import com.mgl.jpa.mapping.samples.support.BaseEntity;
@@ -22,7 +27,20 @@ public class UserProfile extends BaseEntity {
     private ContactInformation contactInformation = new ContactInformation();
 
     @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date signUpTs;
+
+    @NotNull
     @Basic(optional = false)
     private String whatever;
+
+    @NotNull
+    @Size(max = 10)
+    private String shouldNotNull;
+
+    @NotNull
+//    @Basic(optional = false)
+    @Size(max = 12)
+    private String shouldNotNullEither;
 
 }
