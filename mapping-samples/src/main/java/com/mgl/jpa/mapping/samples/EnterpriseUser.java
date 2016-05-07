55c94cc475d049c32ac64c82728fc1170a719fe0
Miguel García Lpez <miguel.garcia.lopez@gmail.com>
2016-05-07 22:17:19 +0200

    Added permissions table to enterprise user.

--- mapping-samples/src/main/java/com/mgl/jpa/mapping/samples/EnterpriseUser.java
+++ mapping-samples/src/main/java/com/mgl/jpa/mapping/samples/EnterpriseUser.java
@@ -1,16 +1,24 @@
 package com.mgl.jpa.mapping.samples;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
+import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 
 import com.mgl.jpa.mapping.samples.tscontrol.HasTsControlFields;
@@ -27,6 +35,8 @@ import lombok.experimental.Delegate;
 @Getter @Setter @ToString(callSuper = true) @NoArgsConstructor
 public class EnterpriseUser extends UserProfile implements HasTsControlFields {
 
+    private static final int PERMISSION_MAX_LEN = 32;
+
     @NotNull
     @Embedded
     @Delegate(types = {HasTsControlFields.class})
@@ -40,6 +50,17 @@ public class EnterpriseUser extends UserProfile implements HasTsControlFields {
     @NotNull
     @Enumerated(EnumType.STRING)
     private RegistrationSource registrationSource;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(
+            name = "EnterpriseUser_Permission",
+            indexes = @Index(columnList = "permission"),
+            joinColumns = @JoinColumn(name = "enterpriseUser"),
+            uniqueConstraints = @UniqueConstraint(
+                    name = "EnterpriseUser_Permission__enterpriseUser_permission_uidx",
+                    columnNames = {"enterpriseUser", "permission"}))
+    @Column(name = "permission", nullable = false, length = PERMISSION_MAX_LEN)
+    private List<String> permissions;
 
     @PrePersist
     public void onPersist() {
