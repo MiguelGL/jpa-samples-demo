6730942490be36dc78e4a75af864a59b9aa25073
Miguel García Lpez <miguel.garcia.lopez@gmail.com>
2016-05-10 09:39:35 +0200

    Added jooq sample.

--- jooq-samples/src/test/java/com/mgl/jpa/mapping/samples/jooq/SampleTest.java
+++ jooq-samples/src/test/java/com/mgl/jpa/mapping/samples/jooq/SampleTest.java
@@ -1,17 +1,32 @@
 package com.mgl.jpa.mapping.samples.jooq;
 
+import static com.mgl.jpa.mapping.samples.jooq.Tables.*;
+
 import com.mgl.jpa.mapping.samples.db.support.BaseTest;
 import com.mgl.jpa.mapping.samples.jooq.tables.daos.CompanyDao;
 import lombok.extern.slf4j.Slf4j;
+import org.jooq.Record2;
+import org.jooq.Result;
 import org.junit.Test;
 
 @Slf4j
 public class SampleTest extends BaseTest {
 
     @Test
-    public void testSample() {
+    public void testBasicJooqSample() {
         CompanyDao companyDao = new CompanyDao(configuration());
         companyDao.findAll().stream().map(String::valueOf).forEach(log::info);
+
+        Result<Record2<String, String>> results = dslContext()
+                .select(COMPANY.ORGANISATIONNAME, USERPROFILE.EMAIL)
+                .from(COMPANY)
+                .join(USERPROFILE)
+                .on(COMPANY.ID.eq(USERPROFILE.COMPANY_ID))
+                .join(REGULARUSER)
+                .on(REGULARUSER.ID.eq(USERPROFILE.ID))
+                .fetch();
+
+        // Loop and process results
     }
 
 }
