a0e05d39b10343e1bf2d917257509cb45ab7c1f5
Miguel García Lpez <miguel.garcia.lopez@gmail.com>
2016-05-10 09:43:22 +0200

    Added querydsl sample test.

--- mapping-samples/src/test/java/com/mgl/jpa/mapping/samples/RegularUserTest.java
+++ mapping-samples/src/test/java/com/mgl/jpa/mapping/samples/RegularUserTest.java
@@ -1,8 +1,11 @@
 package com.mgl.jpa.mapping.samples;
 
 import java.util.Date;
+import java.util.List;
 
 import com.mgl.jpa.mapping.samples.support.BaseJpaTestSupport;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import org.junit.Test;
 
 public class RegularUserTest extends BaseJpaTestSupport {
@@ -26,6 +29,25 @@ public class RegularUserTest extends BaseJpaTestSupport {
         commitAndBeginTransactionAgain();
 
         em().remove(em().find(RegularUser.class, userProfile.getId()));
+    }
+
+    @Test
+    public void testQueryDsl() {
+        QRegularUser qru = QRegularUser.regularUser;
+
+        JPAQuery<RegularUser> query = new JPAQuery<>(em());
+
+        BooleanExpression searchPredicate = qru
+                .contactInformation.email.eq("anemailaddress@gmail.com")
+                .and(qru.company.organisationName.startsWithIgnoreCase("aqua"));
+
+        List<RegularUser> res = query
+                .select(qru)
+                .from(qru)
+                .where(searchPredicate)
+                .fetch();
+
+        // Loop and process results - res
     }
 
 }
